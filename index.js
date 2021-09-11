231e26a747df33c975efe5ba9d3edd16563520d6
xmNuc <xmnuc@o2.pl>
2021-09-11 22:57:22 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const { writeFile, readFile } = require('fs').promises;
+
+const JSON_FILE = './data/input1.json';
+const SUM_FILE = './data/sum.txt';
+
+(async () => {
+  try {
+    const numberFromJson = JSON.parse(await readFile(JSON_FILE, 'utf8'));
+    console.log(numberFromJson);
+
+    const numbersFromAraySummed = numberFromJson.reduce(
+      (accumulator, currentValue) => {
+        console.log('The value of accumulator: ', accumulator);
+        console.log('The value of currentValue: ', currentValue);
+        return accumulator + currentValue;
+      }
+    );
+    console.log(numbersFromAraySummed);
+
+    await writeFile(SUM_FILE, numbersFromAraySummed.toString(), 'utf8');
+    console.log('File is Saved');
+  } catch (error) {
+    console.log('Ou no', error);
+  }
+})();
